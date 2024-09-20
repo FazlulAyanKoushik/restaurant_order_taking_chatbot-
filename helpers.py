76d08775ea39a99afd6f84fa1e5bc84ce804150f
FazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2024-09-20 18:23:05 +0600

    helpers file updated

--- helpers.py
+++ helpers.py
@@ -1,7 +1,6 @@
 """
     This file contains helper functions that are used to interact with the database.
 """
-
 import random
 import string
 from datetime import datetime
@@ -13,6 +12,10 @@ from models import (
 
 
 def insert_into_contacts(name, whatsapp):
+    """
+    This function is used to insert a new contact into the Contacts database.
+    :return: none
+    """
     contact = Contacts(
         name=name,
         whatsapp=whatsapp,
@@ -22,6 +25,10 @@ def insert_into_contacts(name, whatsapp):
 
 
 def insert_into_message(user_number, message, user_type):
+    """
+    This function is used to insert a new message into the Message_db database.
+    :return: none
+    """
     new_msg = Message_db(
         user_number=user_number,
         message=message,
@@ -32,6 +39,10 @@ def insert_into_message(user_number, message, user_type):
 
 
 def generate_random_string(length):
+    """
+    This function is used to generate a random string of a given length.
+    :return: random string
+    """
     characters = string.digits  # Include only digits
     random_string = ''.join(random.choice(characters) for _ in range(length))
     return random_string
